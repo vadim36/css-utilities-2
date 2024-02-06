9bd0f6c8745f6737701d1db8c863a60579f57f51
Vadim Medvedev <vadim130809@gmail.com>
2024-02-06 20:48:42 +0300

    Revert "decompose site"
    This reverts commit e2c291ab9507d462b7955fda2c7853fed791c778.

---
+++ site/scripts/list.ts
@@ -0,0 +1,15 @@
+const backgroundColorHelper:Helper = {
+  shortcut: 'bg',
+  body: 'Helper for background',
+  options: [
+    'primary','secondary','success','danger',
+    'warning','info','light','dark'
+  ],
+  exampleElement: 'div'
+}
+
+const helpers: Helper[] = [
+  backgroundColorHelper
+]
+
+export default helpers
---
+++ site/scripts/types/types.d.ts
@@ -0,0 +1,6 @@
+interface Helper {
+  shortcut:string,
+  body:string,
+  options:string[],
+  exampleElement: string
+}
